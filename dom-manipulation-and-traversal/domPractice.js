ec403b00bc2c597f37b5cf16e36bdec9b3d849e8
Curt Caldwell and James Hanley <james@>
2018-05-30 11:28:48 -0700

    Add answers to practice questions (domPractice.js).

---
+++ dom-manipulation-and-traversal/domPractice.js
@@ -0,0 +1,6 @@
+console.log("What is the difference between .prepend() and .append(). Describe an example of using each.");
+console.log("Prepend adds from top down, append adds bottom up. If you were to prepend text inside an element <p> it would show up from the top down.");
+console.log("What is the difference between .prepend() and .before()? What about .append() and .after()?");
+console.log("Prepend acts on the child of the selected element, before places it before the selected element and not the child.");
+console.log("Write a line of jQuery that selects an element on the DOM with the id of 'salad'.");
+console.log("$(#salad)");
