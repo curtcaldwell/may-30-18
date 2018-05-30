fb2f32ace7c552683fba44594a6cf8d48449ce84
Curt Caldwell and James Hanley <james@>
2018-05-30 13:18:35 -0700

    Add remove function to each li element.

--- dom-manipulation-and-traversal/js/scripts1.js
+++ dom-manipulation-and-traversal/js/scripts1.js
@@ -2,9 +2,21 @@ $(document).ready(function(){
   $("button#bark").click(function(){
     $("ul#dog").prepend("<li>Woof</li>");
     $("ul#cat").prepend("<li>Hiss!</li>");
+    $("ul#cat").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function(){
+      $(this).remove();
+    });
   });
   $("button#meow").click(function(){
     $("ul#cat").prepend("<li>Meow</li>");
     $("ul#dog").prepend("<li>Grrrrr</li>");
+    $("ul#cat").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function(){
+      $(this).remove();
+    });
   });
 });
