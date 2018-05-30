b7aa91739cf7ec460c39775fbddd159cf07aaf35
Curt Caldwell and James Hanley <curt@>
2018-05-30 15:00:01 -0700

    Initialize practice-forms.html, add basic outline

--- dom-manipulation-and-traversal/js/scripts1.js
+++ dom-manipulation-and-traversal/js/scripts1.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
     $("ul#dog").prepend("<li>Woof</li>");
     $("ul#cat").prepend("<li>Hiss!</li>");
     $("ul#cat").children("li").first().click(function(){
+      $(this).parent().before("<p id=\"death\">This animal has been obliterated</p>");
+      $('#death').first().remove().delay(200);
       $(this).remove();
     });
     $("ul#dog").children("li").first().click(function(){
+      $(this).parent().before("<p>This animal has been obliterated</p>");
       $(this).remove();
     });
   });
@@ -13,9 +16,11 @@ $(document).ready(function(){
     $("ul#cat").prepend("<li>Meow</li>");
     $("ul#dog").prepend("<li>Grrrrr</li>");
     $("ul#cat").children("li").first().click(function(){
+      $(this).parent().before("<p>This animal has been obliterated</p>");
       $(this).remove();
     });
     $("ul#dog").children("li").first().click(function(){
+      $(this).parent().before("<p>This animal has been obliterated</p>");
       $(this).remove();
     });
   });
