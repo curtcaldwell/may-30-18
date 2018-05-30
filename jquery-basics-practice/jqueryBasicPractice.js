e18c58760921ada13648068ae5a3919cb0d7f271
Curt Caldwell and James Hanley <curt@>
2018-05-30 08:19:10 -0700

    Answer first set of practice questions for Jquery basic practice.

---
+++ jquery-basics-practice/jqueryBasicPractice.js
@@ -0,0 +1,6 @@
+console.log("What is a function? How do we create one? How do we call one?");
+console.log("A function is a set of operations and peramitors, we create one by initialy calling jquery, and then defining the function and its logic. $('element').eventhandler(function()})");
+console.log("What does it mean when we say 'Business Logic' when referring to JavaScript?");
+console.log("Buisness logic is the back end management of code.");
+console.log("What is the difference between JavaScript and jQuery?");
+console.log("Javascript is the base language and Jquery is a framework for DOM manipulation.");
